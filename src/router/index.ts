df59105efb7ba45798c9d47f0584fe5a43915063
ARonin2001 <aronin.2001@gmail.com>
2024-02-15 16:30:38 +0400

    add router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-// import HomeView from '../views/HomeView.vue';
-import AppVue from '@/App.vue';
+import MainView from '@/views/MainView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: AppVue
+      name: 'main',
+      component: MainView
     }
     // {
     //   path: '/about',
