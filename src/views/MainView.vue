4c79a4478b8d2789147b6d5a8b7eb730c693f048
ARonin2001 <aronin.2001@gmail.com>
2024-02-15 17:30:04 +0400

    MainView

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -5,8 +5,55 @@ import AsideMenu from '@/components/AsideMenu.vue';
 <template>
   <AsideMenu />
   <main class="main">
-    <div class="products">Products</div>
+    <div class="container">
+      <div class="header main__header">
+        <div class="header__title">
+          <div class="header__title_top">
+            <unicon
+              name="shopping-cart"
+              class="title__icon"
+              height="18px"
+              width="18px"
+            />
+            <h1 class="title">Products</h1>
+          </div>
+          <span class="sub-title">guchi</span>
+        </div>
+      </div>
+
+      <RouterView />
+    </div>
   </main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.main {
+  .container {
+    padding: 0 18px;
+  }
+  &__header {
+    margin-bottom: 30px;
+  }
+  .header {
+    &__title {
+      &_top {
+        display: flex;
+        align-items: flex-end;
+
+        .title {
+          font-size: 28px;
+          font-family: 'Roboto-Medium';
+          text-transform: uppercase;
+        }
+      }
+      .sub-title {
+        display: inline-block;
+        font-family: 'Roboto-Light';
+        color: #a4acad;
+        margin-top: 4px;
+        font-size: 16px;
+      }
+    }
+  }
+}
+</style>
