eab2555a68da14318a5f07e881e26d6b435b8083
ARonin2001 <aronin.2001@gmail.com>
2024-02-19 17:59:50 +0400

    api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,29 @@
 import brandsJson from '@/dbJSON/brands.json';
 import productsJson from '@/dbJSON/products.json';
 import { type IBrand } from '@/models/IBrand';
+import type { IProduct } from '@/models/IProduct';
 
-export const apiMethods = {
+export const brandsApi = {
   getBrands: async () => {
-    const result: IBrand[] = await JSON.parse(JSON.stringify(brandsJson));
-    return result;
+    const data: IBrand[] = await JSON.parse(JSON.stringify(brandsJson));
+    return data;
+  },
+  getBrandById: async (brandId: number) => {
+    const data: IBrand[] = await JSON.parse(JSON.stringify(brandsJson));
+
+    return data.filter((b) => b.id === brandId)[0];
+  }
+};
+
+export const productsApi = {
+  getProducts: async () => {
+    const data: IProduct[] = await JSON.parse(JSON.stringify(productsJson));
+
+    return data;
+  },
+  getProductsByBrand: async (brandId: number) => {
+    const data: IProduct[] = await JSON.parse(JSON.stringify(productsJson));
+
+    return data.filter((p) => p.brand === brandId);
   }
 };
